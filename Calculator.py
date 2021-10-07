2e78abe36570f2c841b3030b43ccd5eb8dc0a2f2
creolinho <58535927+creolinho@users.noreply.github.com>
2021-10-07 13:23:15 -0300

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,115 @@
+from tkinter import *
+
+
+root = Tk()
+root.title("Simple Calculator")
+
+#input box
+e = Entry(root, width=35, borderwidth=5)
+e.grid(row=0, column=0, columnspan=3,padx=10,pady=10)  
+
+#functions used in the calculator
+
+def button_click(number):
+    current = e.get()
+    e.delete(0,END)
+    e.insert(0,str(current) + str(number))
+
+
+def button_clear():
+    e.delete(0,END)
+
+def button_add():
+    first_number = e.get()
+    global f_num
+    global math
+    math = "adittion"
+    f_num = int(first_number)
+    e.delete(0,END)
+
+def button_equal():
+    second_num = e.get()
+    e.delete(0,END)
+
+    if math == "adittion":
+        e.insert(0, f_num + int(second_num))
+    elif math == "multiply":
+        e.insert(0, f_num * int(second_num))
+    elif math == "division":
+        e.insert(0, f_num / int(second_num))
+    elif math == "subtraction":
+        e.insert(0, f_num - int(second_num))
+
+def button_subtraction():
+    first_number = e.get()
+    global f_num
+    global math
+    math = "subtraction"
+    f_num = int(first_number)
+    e.delete(0,END)
+
+def button_divide():
+    first_number = e.get()
+    global f_num
+    global math
+    math = "division"
+    f_num = int(first_number)
+    e.delete(0,END)
+
+def button_multiply():
+    first_number = e.get()
+    global f_num
+    global math
+    math = "multiply"
+    f_num = int(first_number)
+    e.delete(0,END)
+
+
+
+#buttons fromm the calculator
+button_1 = Button(root, text="1", padx=40, pady=20,command= lambda: button_click(1))
+button_2 = Button(root, text="2", padx=40, pady=20,command= lambda: button_click(2))
+button_3 = Button(root, text="3", padx=40, pady=20,command= lambda: button_click(3))
+button_4 = Button(root, text="4", padx=40, pady=20,command= lambda: button_click(4))
+button_5 = Button(root, text="5", padx=40, pady=20,command= lambda: button_click(5))
+button_6 = Button(root, text="6", padx=40, pady=20,command= lambda: button_click(6))
+button_7 = Button(root, text="7", padx=40, pady=20,command= lambda: button_click(7))
+button_8 = Button(root, text="8", padx=40, pady=20,command= lambda: button_click(8))
+button_9 = Button(root, text="9", padx=40, pady=20,command= lambda: button_click(9))
+button_0 = Button(root, text="0", padx=40, pady=20,command= lambda: button_click(0))
+
+
+button_add = Button(root, text="+", padx=39, pady=20,command= button_add)
+button_equal = Button(root, text="=", padx=91, pady=20,command= button_equal)
+button_clear = Button(root, text="C", padx=91, pady=20,command= button_clear)
+
+
+button_subtraction = Button(root, text="-", padx=40, pady=20,command= button_subtraction)
+button_multiply = Button(root, text="*", padx=40, pady=20,command= button_multiply)
+button_divide = Button(root, text="/", padx=42, pady=20,command= button_divide)
+
+# defining the buttons place in the calculator GUI
+button_1.grid(row=3,column=0)
+button_2.grid(row=3,column=1)
+button_3.grid(row=3,column=2)
+
+button_4.grid(row=2,column=0)
+button_5.grid(row=2,column=1)
+button_6.grid(row=2,column=2)
+
+button_7.grid(row=1,column=0)
+button_8.grid(row=1,column=1)
+button_9.grid(row=1,column=2)
+
+button_0.grid(row=4,column=0)
+button_add.grid(row=5,column=0)
+button_equal.grid(row=5,column=1, columnspan=2)
+button_clear.grid(row=4,column=1, columnspan=2)
+
+button_subtraction.grid(row=6, column=0)
+button_multiply.grid(row=6, column=1)
+button_divide.grid(row=6, column=2)
+
+
+
+root.mainloop()
